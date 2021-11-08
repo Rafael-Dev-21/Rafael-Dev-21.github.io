56d75af4cf984f79082073512f9dd7f9ca8e42bc
Rafael-Dev-21 <victorrafaelpereiraalves0202@gmail.com>
2021-11-08 13:14:39 -0300

    gsap

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+
+
+$(window).onready(function() {
+  $(window).bind("load", function() {
+    setInterval(function pulselogo() {
+      var tl = gsap.timeline();
+      
+      tl.to("#logo", {
+        duration: .4,
+        scale: 2,
+        repeat: 3,
+        repeatDelay: .4,
+        yoyo: true,
+        ease: "power2.inOut"
+      });
+      
+      return tl;
+    }, 5000);
+    
+    var master = gsap.timeline();
+    master.add(pulselogo());
+  });
+});
