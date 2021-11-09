60b1cdf96d9c33ed88726800c090184690c067dc
Rafael-Dev-21 <victorrafaelpereiraalves0202@gmail.com>
2021-11-09 16:41:56 -0300

    compass

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 http_path = "/"
-sass_dir = "_sass/"
-css_dir = "assets/css/"
-images_dir = "images/"
-javascripts_dir = "assets/js/"
+sass_dir = "./_sass/"
+css_dir = "./assets/css/"
+images_dir = "./images/"
+javascripts_dir = "./assets/js/"
 relative_assets = true
